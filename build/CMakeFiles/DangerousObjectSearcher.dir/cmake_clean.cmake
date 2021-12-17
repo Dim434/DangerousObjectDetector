3791cec37b02c6b42fa5b8009eb0b7cddaa462f9
ddif <potymin.dima@mail.ru>
2021-12-17 20:31:18 +0300

    finished

---
+++ build/CMakeFiles/DangerousObjectSearcher.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DangerousObjectSearcher_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/DangerousObjectSearcher_autogen.dir/ParseCache.txt"
+  "DangerousObjectSearcher_autogen"
+  "CMakeFiles/DangerousObjectSearcher.dir/DangerousObjectSearcher_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/DangerousObjectSearcher.dir/Views/ClickableLabel/ClickableLabel.cpp.o"
+  "CMakeFiles/DangerousObjectSearcher.dir/Views/GUI/GUI.cpp.o"
+  "CMakeFiles/DangerousObjectSearcher.dir/Views/TableWidget/TableWidget.cpp.o"
+  "CMakeFiles/DangerousObjectSearcher.dir/main.cpp.o"
+  "DangerousObjectSearcher"
+  "DangerousObjectSearcher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DangerousObjectSearcher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/DangerousObjectSearcher_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DangerousObjectSearcher_autogen"
+  "DangerousObjectSearcher_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/DangerousObjectSearcher_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
